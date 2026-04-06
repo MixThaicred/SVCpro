dfd9812bfd8e8ee621f3bb174af6cb98ddaee4ce
StriveOps <90435745+StriveOps@users.noreply.github.com>
2026-04-06 17:32:33 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const STRIVE_CONFIG = {
+    firebase: {
+        apiKey: "YOUR_API_KEY",
+        authDomain: "strive-video-center.firebaseapp.com",
+        databaseURL: "https://strive-video-center-default-rtdb.firebaseio.com",
+        projectId: "strive-video-center"
+    }
+};
