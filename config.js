10cc1be563d19199ce2ad963616183b72a472043
StriveOps <90435745+StriveOps@users.noreply.github.com>
2026-04-06 17:39:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const STRIVE_CONFIG = {
     firebase: {
-        apiKey: "YOUR_API_KEY",
+        apiKey: "AIzaSyA0gT_lJAxNDdlYHg7uiU6XUdqSPRgShvs",
         authDomain: "strive-video-center.firebaseapp.com",
         databaseURL: "https://strive-video-center-default-rtdb.firebaseio.com",
         projectId: "strive-video-center"
